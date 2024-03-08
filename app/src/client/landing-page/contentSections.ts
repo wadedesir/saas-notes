0294728e475af3ba5fe5233020eafbc09e0153de
Daniel Cha <cha.danieldev@gmail.com>
2024-03-08 03:17:48 -0500

    danc commit

--- app/src/client/landing-page/contentSections.ts
+++ app/src/client/landing-page/contentSections.ts
@@ -3,10 +3,9 @@ import daBoiAvatar from '../static/da-boi.png';
 import avatarPlaceholder from '../static/avatar-placeholder.png';
 
 export const navigation = [
-  { name: 'Features', href: '#features' },
-  { name: 'Pricing', href: '/pricing' },
-  { name: 'Documentation', href: DOCS_URL },
-  { name: 'Blog', href: BLOG_URL },
+  { name: 'Notes', href: '#features' },
+  { name: 'Sign-Up', href: '/signup' },
+  { name: 'Log-In', href: '/login' },
 ];
 export const features = [
   {
